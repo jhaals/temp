e5549a2756de36e75125dc29cf7c4fdb8906408a
Johan Haals <johan.haals@gmail.com>
2018-11-16 14:04:57 +0800

    Remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,12 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Create from './Create';
 
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
-
 class App extends React.Component {
   public render() {
     return (
       <Router>
         <div>
           <Route path="/" exact={true} component={Create} />
-          <Route path="/about/" component={About} />
-          <Route path="/users/" component={Users} />
         </div>
       </Router>
     );
